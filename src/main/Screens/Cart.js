baeea8c3dc037e9e86d2006d38671c486ad66992
Faiez waseem <78906502+FaiezWaseem@users.noreply.github.com>
2022-03-02 14:21:20 +0500

    cart Total Calculation with tax

--- src/main/Screens/Cart.js
+++ src/main/Screens/Cart.js
@@ -5,7 +5,7 @@ import CartItem from "../component/cart/CartItem"
 import ItemTotal from "../component/cart/ItemTotal"
 import { useRecoilState } from "recoil";
 import totalItems from "../component/cart/cartLogic" 
-
+import cart from "../assets/images/cart.jpg"
 export default function Cart() {
   const [items] =  useRecoilState(totalItems)
   return (
@@ -14,10 +14,14 @@ export default function Cart() {
        <div  className='container mt-5 mb-5'>
            <div  className='row'>
                <div className='col-lg-8'>
-                 {
+                 { items.length > 0 ?
                    items.map((item)=>{
                    return <CartItem  obj={item} key={item.id} />
-                   })
+                   }) :
+                   <div  className='card card-body mb-3' style={{ alignItems : "center", justifyContent : "center"}}>
+                     <h3>No Items in Cart</h3>
+                     <img  src={cart}  alt="No items in cart" style={{ width : 250 , height : 250}}  />
+                   </div>
                  }
                </div>
                <div className='col-lg-4'>
--- src/main/component/cart/ItemTotal.jsx
+++ src/main/component/cart/ItemTotal.jsx
@@ -6,12 +6,14 @@ import totalItems from "./cartLogic"
 export default function ItemTotal() {
     const [items] =  useRecoilState(totalItems)
     const [total , setTotal] = React.useState(0);
+    const [totalWithTax , settotalWithTax] = React.useState(0);
     React.useEffect(()=>{
       var calculate = 0;
         items.forEach(item =>{
             calculate += item.qty * item.price;
             setTotal(calculate) 
         })
+        settotalWithTax(calculate + +(0.10 * calculate).toFixed(2) )
         if(items.length === 0){
          setTotal(0) 
         }
@@ -36,18 +38,15 @@ export default function ItemTotal() {
              <dt>Total price:</dt>
              <dd className="text-end"> ${total}</dd>
           </dl>
-          <dl className="dlist-align">
-             <dt>Discount:</dt>
-             <dd className="text-end text-success"> - $60.00 </dd>
-          </dl>
+     
           <dl className="dlist-align">
              <dt>TAX:</dt>
-             <dd className="text-end text-danger"> + $14.00 </dd>
+             <dd className="text-end text-danger"> + ${(0.10 * total).toFixed(2)} </dd>
           </dl>
           <hr />
           <dl className="dlist-align">
              <dt>Total:</dt>
-             <dd className="text-end text-dark h5"> $1357.97 </dd>
+             <dd className="text-end text-dark h5"> ${totalWithTax} </dd>
           </dl>
           <div className="d-grid gap-2 my-3"> 
           <a className="btn btn-primary w-100"> Make Purchase </a> 
@@ -58,4 +57,12 @@ export default function ItemTotal() {
 
  </aside></div>
   )
+}
+function discountCard() {
+   return (
+      <dl className="dlist-align">
+      <dt>Discount:</dt>
+      <dd className="text-end text-success"> - $60.00 </dd>
+   </dl>
+   )
 }
